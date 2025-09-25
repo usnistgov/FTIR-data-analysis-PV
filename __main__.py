6ff236110f73ca40fa2f55e8be984536e99e6a61
karissajensen <Karissa.jensen.neill@gmail.com>
2025-09-25 13:03:20 -0400

    python environment fix attempt

--- __main__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Sep 11 12:29:41 2025
-
-@author: klj
-"""
-
-from .FTIR_baseline_polyfit import baselinePolyFitLoop
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from ftir-data-analysis-pv!")
+
+
+if __name__ == "__main__":
+    main()
